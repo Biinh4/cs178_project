81863cdb1109e88b1bf333f9bb67bbb60f28440d
Nguyen Hoang <Nguyeh4@uci.edu>
2025-03-17 22:32:39 -0700

    this is all i got

--- cnn.py
+++ cnn.py
@@ -7,56 +7,41 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
 
 
-class CNN:
+class CNN(object):
     def __init__(self):
-        # Initialize parameters: assumes data size! 28x28 and 10 classes
-        self.conv_ = torch.nn.Conv2d(3, 16, (5,5), stride=2)  # Be careful when declaring sizes;
-        self.pool_ = torch.nn.MaxPool2d(3, stride=2)          # inconsistent sizes will give you
-        self.lin_ = torch.nn.Linear(576,10)                   # hard-to-read error messages.
+        # Updated for 3-channel 32x32 images
+        self.conv_ = torch.nn.Conv2d(3, 16, (5,5), stride=2)  
+        self.pool_ = torch.nn.MaxPool2d(3, stride=2)          
+        self.lin_ = torch.nn.Linear(576, 10)  # Adjusted for new feature size
             
-    def forward_(self,X):
-        """Compute NN forward pass and output class probabilities (as tensor) """
-        r1 = self.conv_(X)             # X is (m,1,28,28); R is (m,16,24,24)/2 = (m,16,12,12)
-        h1 = torch.relu(r1)            #
-        h1_pooled = self.pool_(h1)     # H1 is (m,16,12,12), so H1p is (m,16,10,10)/2 = (m,16,5,5)
-        h1_flat = torch.nn.Flatten()(h1_pooled)  # and H1f is (m,400)
+    def forward_(self, X):
+        """Compute NN forward pass and output class probabilities"""
+        r1 = self.conv_(X)          
+        h1 = torch.relu(r1)         
+        h1_pooled = self.pool_(h1)  
+        h1_flat = torch.nn.Flatten()(h1_pooled)  
         r2 = self.lin_(h1_flat)
-        f  = torch.softmax(r2, dim=1)  # Output is (m,10)
+        f  = torch.softmax(r2, dim=1)  
         return f 
         
     def parameters(self): 
-        return list(self.conv_.parameters())+list(self.pool_.parameters())+list(self.lin_.parameters())
+        return list(self.conv_.parameters()) + list(self.lin_.parameters())
     
-    def predict(self,X):
-        """Compute NN class predictions (as array) """
-        # m = X.shape[3]
-        # n = X.shape[0] * X.shape[1] * X.shape[2]
+    def predict(self, X):
+        """Compute NN class predictions"""
+        Xtorch = torch.tensor(X, dtype=torch.float32).permute(0, 3, 1, 2)
+        return np.argmax(self.forward_(Xtorch).detach().numpy(), axis=1)
 
-        # Xtorch = torch.tensor(X).reshape(m,1,int(np.sqrt(n)),int(np.sqrt(n)))
-        # return self.classes_[np.argmax(self.forward_(Xtorch).detach().numpy(),axis=1)]   # pick the most probable class
+    def J01(self, X, y):  
+        return (y != self.predict(X)).mean()
 
-        # Had to change the order of the X
-        Xtorch = torch.tensor(X).permute(0, 3, 1, 2).float()  # (32,32,3,26032) → (26032,3,32,32)
-        return self.classes_[np.argmax(self.forward_(Xtorch).detach().numpy(), axis=1)]
+    def JNLL_(self, X, y):  
+        return (y != self.predict(X)).mean()
 
-    def J01(self,X,y):   return (y != self.predict(X)).mean()
-
-    def JNLL_(self,X,y): 
-        # Converts our classes from 1-10 to 0-9
-        y = torch.tensor(y).long() - 1
-        return -torch.log(self.forward_(X)[range(len(y)),y.long()]).mean()
-
-    def fit(self, X,y, batch_size=256, max_iter=100, learning_rate_init=.005, momentum=0.9, alpha=.001, plot=False):
+    def fit(self, X, y, batch_size=256, max_iter=100, learning_rate_init=0.005, plot=False):
         self.classes_ = np.unique(y)        
-        # m = X.shape[3]
-        # n = X.shape[0] * X.shape[1] * X.shape[2]
-        
-        # Xtorch = torch.tensor(X).reshape(m,1,int(np.sqrt(n)),int(np.sqrt(n)))  
-        # self.loss01, self.lossNLL = [self.J01(X,y)], [float(self.JNLL_(Xtorch,y))]
 
-        # Reshaping our data
-        Xtorch = torch.tensor(X).permute(0, 3, 1, 2).float()
-        # print(Xtorch.shape)
+        Xtorch = torch.tensor(X).permute(0, 3, 1, 2).float()  # Reshape correctly
         ytorch = torch.tensor(y, dtype=torch.long)
 
         self.loss01, self.lossNLL = [self.J01(X, y)], [float(self.JNLL_(Xtorch, ytorch))]
@@ -64,20 +49,19 @@ class CNN:
         optimizer = torch.optim.Adam(self.parameters(), lr=learning_rate_init)
 
         m = X.shape[0]
-        for epoch in range(max_iter):                        # 1 epoch = pass through all data
-            pi = np.random.permutation(m)                    # per epoch: permute data order
-            for ii,i in enumerate(range(0,m,batch_size)):    # & split into mini-batches
+        for epoch in range(max_iter):                        
+            pi = np.random.permutation(m)                   
+            for i in range(0, m, batch_size):    
                 ivals = pi[i:i+batch_size]
-                optimizer.zero_grad()                        # Reset the gradient computation
-                Ji = self.JNLL_(Xtorch[ivals,:,:,:],y[ivals])
+                optimizer.zero_grad()                        
+                Ji = self.JNLL_(Xtorch[ivals], ytorch[ivals])
                 Ji.backward()
                 optimizer.step()
-            self.loss01.append(self.J01(X,y))                # track 0/1 and NLL losses
-            self.lossNLL.append(float(self.JNLL_(Xtorch,y)))
-            
-            if plot:                                         # optionally visualize progress
-                display.clear_output(wait=True)
-                plt.plot(range(epoch+2),self.loss01,'b-',range(epoch+2),self.lossNLL,'c-')
+            self.loss01.append(self.J01(X, y))              
+            self.lossNLL.append(float(self.JNLL_(Xtorch, ytorch)))
+
+            if plot:                                         
+                plt.plot(range(epoch+2), self.loss01, 'b-', range(epoch+2), self.lossNLL, 'c-')
                 plt.title(f'J01: {self.loss01[-1]}, NLL: {self.lossNLL[-1]}')
                 plt.draw(); plt.pause(.01)
 
@@ -104,6 +88,9 @@ class Represent:
 
         print(f"\nClasses: {np.unique(self.validation_data_y)}")
 
+    def play_data(self):
+        print(self.training_data_X.shape)
+        print(self.training_data_y[0])
 
     # Show our data
     def histogram_training_labels(self):
@@ -123,22 +110,26 @@ class Represent:
 
 
     def train_epochs(self):
-        epochs = [1, 10, 100, 150, 200, 250]
+        # epochs = [1, 10, 50, 75, 100, 125]
+        epochs = [25, 50]
 
         tr_err_rate = []
         ve_err_rate = []
 
         for epoch in epochs:
+            print(epoch)
             cnn = CNN()
-            cnn.fit(self.training_data_X, self.training_data_y, max_iter=epoch)
+            cnn.fit(self.training_data_X[:1000], self.training_data_y[:1000], max_iter=epoch)
 
-            tr_pred = cnn.predict(self.training_data_X)
-            ve_pred = cnn.predict(self.validation_data_X)
+            tr_pred = cnn.predict(self.training_data_X[:1000])
+            ve_pred = cnn.predict(self.validation_data_X[:1000])
 
-            tr_err_rate.append(np.mean(tr_pred != self.training_data_y))
-            ve_err_rate.append(np.mean(ve_pred != self.validation_data_y))
+            tr_err_rate.append(np.mean(tr_pred != self.training_data_y[:1000]))
+            ve_err_rate.append(np.mean(ve_pred != self.validation_data_y[:1000]))
 
+            print(tr_pred)
         # Plot training and validation error rates
+
         plt.figure(figsize=(8, 5))
         plt.plot(epochs, tr_err_rate, marker='o', label='Training Error')
         plt.plot(epochs, ve_err_rate, marker='x', linestyle='dashed', label='Validation Error')
@@ -149,7 +140,8 @@ class Represent:
         plt.legend()
         plt.grid(True)
         plt.show()
-
+        print(tr_err_rate)
+        print(ve_err_rate)
         return tr_err_rate, ve_err_rate
 
     
@@ -185,7 +177,7 @@ class Represent:
     # MOST EXPENSIVE
     def train_optimally(self):
         learning_rates = [0.001, 0.01, 0.025, 0.05, 0.075, 0.1]
-        epochs = [1, 10, 100, 150, 200, 250]
+        epochs = [1, 10, 50, 75, 100, 125]
         
     
         results = {}
--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import scipy.io
 
 def main():
     represent = cnn.Represent(scipy.io.loadmat("test_32x32.mat"), scipy.io.loadmat("test_32x32.mat"))
+    # represent.play_data()
     represent.train_epochs()
-    represent.train_learning_rate()
-    represent.train_optimally()
+    # represent.train_learning_rate()
+    # represent.train_optimally()
     
 
     # represent.histogram()
