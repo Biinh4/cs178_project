b5c94e7188f8dff15f7753c03c6d3b0c72599761
Nguyen Hoang <Nguyeh4@uci.edu>
2025-03-17 22:40:10 -0700

    RESET

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-import cnn
+import cs178_project.project as project
 import scipy.io
 
 
 def main():
-    represent = cnn.Represent(scipy.io.loadmat("test_32x32.mat"), scipy.io.loadmat("test_32x32.mat"))
-    # represent.play_data()
-    represent.train_epochs()
-    # represent.train_learning_rate()
-    # represent.train_optimally()
-    
-
-    # represent.histogram()
-
+    pass
 
 if __name__ == "__main__":
     main()
